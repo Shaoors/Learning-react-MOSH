5565c8be6ef357ebeb255c86c55f9edac0dda463
shaoors <shaoors97@gmail.com>
2021-06-03 18:56:32 +0500

    till removing Local state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 1,
-  };
   //   constructor() {
   //     super();
   //     console.log(this);
   //   }
-  handleIncreement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+
+  // handleIncreement = () => {
+  //   this.setState({ value: this.props.counter.value + 1 });
+  // };
 
   render() {
     return (
@@ -18,22 +16,28 @@ class Counter extends Component {
         <span className={this.classesForSpan()}>{this.formatCount()}</span>
         <button
           className="btn btn-secondary btn-sm"
-          onClick={this.handleIncreement}
+          onClick={() => this.props.onIncreement(this.props.counter)}
         >
           Increement
+        </button>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
         </button>
       </div>
     );
   }
   classesForSpan() {
     let classes = "badge  m-2 bg-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
